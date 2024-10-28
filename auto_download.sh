076c505bde8ed13c42c9169a35101bc308553ebe
weibingwen-uav009 <pancake.wbw@gmail.com>
2024-10-28 14:45:06 +0800

    add git lfs pull command to make sure download completely

--- auto_download.sh
+++ auto_download.sh
@@ -3,8 +3,17 @@ mkdir ../rdt/
 cd ../rdt/
 git lfs install
 git clone git@hf.co:robotics-diffusion-transformer/rdt-1b
+cd rdt-1b 
+git lfs pull
+cd ..
 git clone git@hf.co:google/t5-v1_1-xxl
+cd t5-v1_1-xxl
+git lfs pull
+cd ..
 git clone git@hf.co:google/siglip-so400m-patch14-384
+cd siglip-so400m-patch14-384
+git lfs pull
+cd ..
 ### omnigibson
 
 
