19bd977a10df9634c4b9c4c6b8ce27b2eb2750e0
weibingwen-uav009 <pancake.wbw@gmail.com>
2024-10-28 14:41:40 +0800

    create auto_download.sh fro downloading rdt

---
+++ auto_download.sh
@@ -0,0 +1,12 @@
+### rdt
+mkdir ../rdt/
+cd ../rdt/
+git lfs install
+git clone git@hf.co:robotics-diffusion-transformer/rdt-1b
+git clone git@hf.co:google/t5-v1_1-xxl
+git clone git@hf.co:google/siglip-so400m-patch14-384
+### omnigibson
+
+
+
+
