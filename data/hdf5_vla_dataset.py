c8e8d9ecbf4a11f313b288144dce2a2cf787d860
weibingwen-txair <pacnake.wbw@gamil.com>
2024-11-13 15:31:19 +0800

    [update] add wash_cup_1

--- data/hdf5_vla_dataset.py
+++ data/hdf5_vla_dataset.py
@@ -18,8 +18,11 @@ class HDF5VLADataset:
     def __init__(self) -> None:
         # [Modify] The path to the HDF5 dataset directory
         # Each HDF5 file contains one episode
+
         HDF5_DIR = "data/datasets/close_laptop/"
         self.DATASET_NAME = "close_laptop"
+        # HDF5_DIR = "data/datasets/wash_cup_1/"
+        # self.DATASET_NAME = "wash_cup_1"
         
         self.file_paths = []
         for root, _, files in os.walk(HDF5_DIR):
